ca995dbe6f949319c2c7fbffae9832e73870c827
Matthieu Schulz <m.schulz18@ejm.org>
2020-09-13 20:24:31 +0100

    update

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -6,7 +6,7 @@ var plugins = [{
       options: {"plugins":[]},
     },{
       plugin: require('/Users/matthieuschulz/CS/matthieuschulz.github.io/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
-      options: {"plugins":[],"name":"Matthieu Schulz","short_name":"Matthieu Schulz","start_url":"/","background_color":"#6D83F2","theme_color":"#6D83F2","display":"standalone","icon":"src/static/logo.svg"},
+      options: {"plugins":[],"name":"Matthieu Schulz","short_name":"Matthieu Schulz","start_url":"/","background_color":"#6D83F2","theme_color":"#6D83F2","display":"standalone","icon":"src/static/logo.svg","cache_busting_mode":"query","include_favicon":true,"legacy":true,"theme_color_in_head":true,"cacheDigest":"7f398037bc27f27801fe46dc6a7ac5f2"},
     },{
       plugin: require('/Users/matthieuschulz/CS/matthieuschulz.github.io/node_modules/gatsby-plugin-google-analytics/gatsby-ssr'),
       options: {"plugins":[],"trackingId":"UA-119972196-1","head":true},
